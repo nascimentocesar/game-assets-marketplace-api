5f28c351953fca9597f0f3ef4c0d92ba5fd5e52e
Cesar Nascimento <dev.cesar.nascimento+gh@gmail.com>
2025-02-18 14:07:16 -0300

    chore: configure nginx reverse proxy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
+import os from "os";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("respond with a resource");
+  res.send(`I am ${os.hostname()}`);
 });
 
 export default router;
