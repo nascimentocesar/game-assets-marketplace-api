62c9c2e62a3486602ce2ce894b046674ed3d85bd
Cesar Nascimento <dev.cesar.nascimento+gh@gmail.com>
2025-03-31 17:36:11 -0300

    chore: add support to multiple docker container environments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,21 @@
-FROM node:23-alpine
+FROM node:23-alpine AS build
 WORKDIR /app
 RUN corepack enable
-COPY package.json pnpm-lock.yaml ./
+COPY package.json pnpm-lock.yaml tsconfig.json ./
 RUN pnpm install --frozen-lockfile
 COPY ./prisma ./prisma
 RUN pnpm exec prisma generate
 COPY ./src ./src
+
+FROM build AS dev
 CMD ["pnpm", "start"]
+
+FROM build AS prd
+RUN pnpm build
+RUN pnpm install -P --frozen-lockfile \
+  && rm -Rf ./src \
+  && rm tsconfig.json
+CMD ["node", "./src/bin/www.ts"]
+
+FROM build AS test
+CMD ["pnpm", "jest"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@
 echo -e "📦 Creating \e[32m.env\e[0m file..."
 cp .env.example .env
 echo -e "📦 Building \e[32mdocker containers\e[0m..."
-docker compose up -d --build
+docker compose --profile dev up -d --build
 echo -e "📦 Running \e[32mdatabase migrations\e[0m..."
 sleep 5
-docker exec -it $(docker ps -aqf name=game-assets-marketplace-api-express | head -n 1) pnpm exec prisma migrate dev
+docker exec -it game-assets-marketplace-api-express-dev-1 pnpm prisma migrate dev
 echo "✅ Build complete"
-echo -e "🚀 Application is running at \e[32mhttp://localhost\e[0m"
+echo -e "🚀 Application is running at \e[32mhttp://localhost:3000\e[0m"
